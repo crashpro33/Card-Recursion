4d1f2cb720781e2e407a63d0c08aa5f11484c0db
Matthew King <32280053+crashpro33@users.noreply.github.com>
2019-04-30 18:30:17 -0500

    Deal.java
    Just a snippet of the table panel I used for the game. Adds cards in a downward diagonal angle.

---
+++ Deal.java
@@ -0,0 +1,37 @@
+  // Recursion for placing cards
+	public void Deal(int col) {
+		// Incrementing recursion
+		int col2 = col;
+		if (col == 7) {
+			return;
+		}
+		else {
+			// Using the recursive variable for how many cards per column
+			for (; col < column.length; col++) {
+				Card card = deck.deal();
+				
+				// Move kings to top (King is 12)
+				if (card.getValue() == 12) {
+					column[col].addToBeginning(card);
+				}
+				else {
+					column[col].add(card);
+				}
+			}
+		}
+		// Increment the recursive variable
+		col2++;
+		// Recurse
+		Deal(col2);
+	}
+  
+  
+  public void addToBeginning(Card card) {
+		card.setXY(stackX, stackY);
+		cards.add(0, card);
+		
+		for (int i = 1; i < cards.size(); i++) {
+			Card nextCard = cards.get(i);
+			nextCard.addToXY(0, overlap);
+		}
+	}
